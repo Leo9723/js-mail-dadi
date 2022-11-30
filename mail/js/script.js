157480b1e818e967ce65c94526d672578bb3fe31
Leo9723 <pala9761@gmail.com>
2022-11-30 13:58:22 +0100

    aggiunta bonus esercizio delle mail

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,18 +1,23 @@
-const array = ['harrypotter@gmail.com', 'frodobaggins@gmail.com', 'sauronsignoredimordor@icloud.com', 'albusparcifalwulfricbryansilente@outlook.com', 'jonsnow@winterfell.we']
+const button = document.getElementById('button')
 
-let user_mail = prompt('inserisci la tua mail')
+button.addEventListener('click', function(){
 
-let flag = false;
-for(let i=0; i<array.length; i++){
-    if(array[i] == user_mail){
-        flag = true;
+    let user_mail = document.getElementById('user_mail').value
+
+    const array = ['harrypotter@gmail.com', 'frodobaggins@gmail.com', 'sauronsignoredimordor@icloud.com', 'albusparcifalwulfricbryansilente@outlook.com', 'jonsnow@winterfell.we']
+
+    let flag = false;
+    for(let i=0; i<array.length; i++){
+        if(array[i] == user_mail){
+            flag = true;
+        }
     }
-}
-
-if(flag){
-    alert('la tua mail è presente')
-}
-
-else{
-    alert('la tua mail non è presente')
-}
+    
+    if(flag){
+        document.getElementById('esit').innerHTML = 'la tua mail è presente tra le mail autorizzate'
+    }
+    
+    else{
+        document.getElementById('esit').innerHTML = 'la tua mail non è presente tra le mail autorizzate'
+    }
+})
